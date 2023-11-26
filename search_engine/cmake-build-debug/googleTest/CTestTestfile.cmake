1fdcc456788845456ddf5043357fde666c7ef650
justVladislav <vlad.gkl1306@gmail.com>
2023-11-26 20:28:43 +0100

    Folder was removed

--- search_engine/cmake-build-debug/googleTest/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Dev/ProbePioneer/search_engine/googleTest
-# Build directory: D:/Dev/ProbePioneer/search_engine/cmake-build-debug/googleTest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/Dev/ProbePioneer/search_engine/cmake-build-debug/googleTest/GoogleTest[1]_include.cmake")
-subdirs("../_deps/googletest-build")
